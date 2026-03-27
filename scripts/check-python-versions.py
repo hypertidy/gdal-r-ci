c69395b559e76478b231f0710aa447e724b48e95
Michael Sumner <mdsumner@gmail.com>
2026-03-27 20:15:56 +0000

    comments

--- scripts/check-python-versions.py
+++ scripts/check-python-versions.py
@@ -1,8 +1,14 @@
 from osgeo import gdal, ogr, osr
 import rasterio, fiona, pyproj, shapely, geopandas
-print('GDAL:      ', gdal.__version__)
-print('rasterio:  ', rasterio.__version__)
-print('fiona:     ', fiona.__version__)
-print('pyproj:    ', pyproj.__version__)
-print('shapely:   ', shapely.__version__)
-print('geopandas: ', geopandas.__version__)
+import xarray, zarr, fsspec, netCDF4, h5py
+
+print('osgeo.gdal: ', gdal.__version__)
+print('rasterio:   ', rasterio.__version__)
+print('fiona:      ', fiona.__version__)
+print('pyproj:     ', pyproj.__version__)
+print('shapely:    ', shapely.__version__)
+print('geopandas:  ', geopandas.__version__)
+print('xarray:     ', xarray.__version__)
+print('zarr:       ', zarr.__version__)
+print('netCDF4:    ', netCDF4.__version__)
+print('h5py:       ', h5py.__version__)
