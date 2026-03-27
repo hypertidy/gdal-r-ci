6b9cf96b03deea8cad4c602aca96e4b18765b64b
Michael Sumner <mdsumner@gmail.com>
2026-03-27 19:51:33 +0000

    quoting

---
+++ scripts/check-python-versions.py
@@ -0,0 +1,8 @@
+from osgeo import gdal, ogr, osr
+import rasterio, fiona, pyproj, shapely, geopandas
+print('GDAL:      ', gdal.__version__)
+print('rasterio:  ', rasterio.__version__)
+print('fiona:     ', fiona.__version__)
+print('pyproj:    ', pyproj.__version__)
+print('shapely:   ', shapely.__version__)
+print('geopandas: ', geopandas.__version__)
