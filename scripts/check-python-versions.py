ed043781d099bb0dce87080aa58c73cded6e20e1
Michael Sumner <mdsumner@gmail.com>
2026-03-28 02:04:33 +0000

    moar python

--- scripts/check-python-versions.py
+++ scripts/check-python-versions.py
@@ -1,14 +1,34 @@
 from osgeo import gdal, ogr, osr
 import rasterio, fiona, pyproj, shapely, geopandas
 import xarray, zarr, fsspec, netCDF4, h5py
+import pyarrow, polars
+import numpy, pandas, scipy
 
-print('osgeo.gdal: ', gdal.__version__)
-print('rasterio:   ', rasterio.__version__)
-print('fiona:      ', fiona.__version__)
-print('pyproj:     ', pyproj.__version__)
-print('shapely:    ', shapely.__version__)
-print('geopandas:  ', geopandas.__version__)
-print('xarray:     ', xarray.__version__)
-print('zarr:       ', zarr.__version__)
-print('netCDF4:    ', netCDF4.__version__)
-print('h5py:       ', h5py.__version__)
+# Critical: osgeo and rasterio must agree on GDAL version
+osgeo_ver = gdal.__version__
+rasterio_gdal = rasterio.__gdal_version__
+
+print('=== Version check ===')
+print(f'osgeo.gdal:    {osgeo_ver}')
+print(f'rasterio GDAL: {rasterio_gdal}')
+
+if osgeo_ver != rasterio_gdal:
+    print(f'WARNING: GDAL version mismatch! osgeo={osgeo_ver} rasterio={rasterio_gdal}')
+else:
+    print('GDAL alignment: OK')
+
+print()
+print('=== Package versions ===')
+print(f'numpy:      {numpy.__version__}')
+print(f'rasterio:   {rasterio.__version__}')
+print(f'fiona:      {fiona.__version__}')
+print(f'pyproj:     {pyproj.__version__}')
+print(f'shapely:    {shapely.__version__}')
+print(f'geopandas:  {geopandas.__version__}')
+print(f'xarray:     {xarray.__version__}')
+print(f'zarr:       {zarr.__version__}')
+print(f'netCDF4:    {netCDF4.__version__}')
+print(f'h5py:       {h5py.__version__}')
+print(f'pyarrow:    {pyarrow.__version__}')
+print(f'polars:     {polars.__version__}')
+print(f'pandas:     {pandas.__version__}')
